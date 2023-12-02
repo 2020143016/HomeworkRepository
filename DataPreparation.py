f23087b58c30787c9d795450257044c79b8b091f
2020143016 <peterjang@yonsei.ac.kr>
2023-12-02 16:47:16 +0900

    now program process data and delete un-needed columns

--- DataPreparation.py
+++ DataPreparation.py
@@ -1,5 +1,8 @@
 #python version 3.9.12 64bit
+from ast import Delete
+from statistics import mode
 import pandas as pd
+import os
 def CountryExtractor(ListToExtract):# return(list of country, number of country included)
     ListOfCountry = []
     for i in range(len(ListToExtract)):
@@ -12,6 +15,14 @@ def ExtractSameCountry(Target_list):
         if i in CountryExtractor(Target_list)[0]:
             lst.append(i)
     return lst
+def DataProcessing(PandasData):
+    lst = ['LOCATION','TIME','Value']
+    lstOfColumns = []
+    for i in range(len(PandasData.columns)):
+        if not(PandasData.columns[i] in lst):
+            lstOfColumns.append(PandasData.columns[i])
+    PDD = PandasData.drop(lstOfColumns,axis=1)
+    return PDD
 csv_TrustInGovernment = pd.read_csv("Dataset_Trust_in_government.csv")
 csv_MultifactorProductivity = pd.read_csv("Dataset_Multifactor_productivity.csv")
 csv_GrossDomesticProduct = pd.read_csv("Dataset_Gross_domestic_product.csv")
@@ -20,15 +31,34 @@ csv_PriceLevelIndices = pd.read_csv("Dataset_Price_level_indices.csv")
 #print(csv_MultifactorProductivity)
 #print(csv_GrossDomesticProduct)
 #print(csv_PriceLevelIndives)
+
+# pandas to numpy
 csv_TrustInGovernment_np = csv_TrustInGovernment.to_numpy()
 csv_MultifactorProductivity_np = csv_MultifactorProductivity.to_numpy()
 csv_GrossDomesticProduct_np = csv_GrossDomesticProduct.to_numpy()
 csv_PriceLevelIndices_np = csv_PriceLevelIndices.to_numpy()
-print(CountryExtractor(csv_TrustInGovernment_np)[1])
-print(CountryExtractor(csv_MultifactorProductivity_np)[1])
-print(CountryExtractor(csv_GrossDomesticProduct_np)[1])
-print(CountryExtractor(csv_PriceLevelIndices_np)[1])
-print("")
-print(CountryExtractor(csv_MultifactorProductivity_np)[1] - len(ExtractSameCountry(csv_MultifactorProductivity_np)))
-print(CountryExtractor(csv_GrossDomesticProduct_np)[1] - len(ExtractSameCountry(csv_GrossDomesticProduct_np)))
-print(CountryExtractor(csv_PriceLevelIndices_np)[1] - len(ExtractSameCountry(csv_PriceLevelIndices_np)))
+# exsisting country
+TIG_Country = CountryExtractor(csv_TrustInGovernment_np)[0]
+MP_Contry = CountryExtractor(csv_MultifactorProductivity_np)[0]
+GDP_Country = CountryExtractor(csv_GrossDomesticProduct_np)[0]
+PLI_Country = CountryExtractor(csv_PriceLevelIndices_np)[0]
+
+#print(csv_TrustInGovernment)
+#print(csv_MultifactorProductivity)
+#print(csv_GrossDomesticProduct)
+#print(csv_PriceLevelIndices)
+
+#delete column except time,location, value
+TIG_CoulumnExtract = DataProcessing(csv_TrustInGovernment)
+MP_CoulumnExtract = DataProcessing(csv_MultifactorProductivity)
+GDP_CoulumnExtract = DataProcessing(csv_GrossDomesticProduct)
+PLI_CoulumnExtract = DataProcessing(csv_PriceLevelIndices)
+
+print(TIG_CoulumnExtract)
+print(MP_CoulumnExtract)
+print(GDP_CoulumnExtract)
+print(PLI_CoulumnExtract)
+TIG_CoulumnExtract.to_csv("ProcessingDataset_Trust_in_government.csv",mode='w',index=False)
+MP_CoulumnExtract.to_csv("ProcessingDataset_Multifactor_productivity.csv",mode='w',index=False)
+GDP_CoulumnExtract.to_csv("ProcessingDataset_Gross_domestic_product.csv",mode='w',index=False)
+PLI_CoulumnExtract.to_csv("ProcessingDataset_Price_level_indices.csv",mode='w',index=False)
